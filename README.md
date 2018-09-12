be88b0425f99dc76d63fbe7eac3bdcee4edba981
Simon HV <simon.viet@hawk-platform.io>
2018-09-12 13:20:41 +0200

    handle duplicate keys

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Ce script Node.js permet d'ajouter automatiquement des traductions dans les fichiers `en.json` et `fr.json` et se charge également de les trier par ordre alphabétique.
 
+Il y a également une gestion des doublons qui demande confirmation avant d'écraser la clé déjà existante.
+
 ## Installation
 
 - Cloner ce repository
@@ -11,4 +13,4 @@ Ce script Node.js permet d'ajouter automatiquement des traductions dans les fich
 
 L'appli se lance, il ne reste plus qu'à entrer les clés de traductions :
 
-![Prévisualisation](https://image.ibb.co/mcGCzp/Capture_d_e_cran_2018_09_12_a_11_38_05.png)
+![Prévisualisation](https://image.ibb.co/nf9ch9/Capture_d_e_cran_2018_09_12_a_13_19_38.png)
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const fs = require('fs');
-const sortJson = require('sort-json');
-const inquirer = require('inquirer');
+const fs = require('fs')
+const sortJson = require('sort-json')
+const inquirer = require('inquirer')
 
-const options = { ignoreCase: true, reverse: false, depth: 1 };
+const options = { ignoreCase: true, reverse: false, depth: 1 }
 
-const translationFilesLocation = '/Users/simon/Documents/Projets/manager-front-app/src/assets/i18n'; // Change your translations files location here
+const translationFilesLocation = '/Users/simon/Documents/Projets/manager-front-app/src/assets/i18n' // Change your translations files location here
 
 
 let dataFr = JSON.parse(fs.readFileSync(`${translationFilesLocation}/fr.json`))
@@ -32,15 +32,39 @@ const promptUser = [
 
 const askTranslation = () => {
   inquirer.prompt(promptUser).then((res) => {
-    const key = res['key'];
-    dataFr[key] = res['fr'];
-    dataEn[key] = res['en'];
+    const key = res['key']
 
-    fs.writeFileSync(`${translationFilesLocation}/fr.json`, JSON.stringify(dataFr));
-    fs.writeFileSync(`${translationFilesLocation}/en.json`, JSON.stringify(dataEn));
-    sortJson.overwrite([`${translationFilesLocation}/fr.json`, `${translationFilesLocation}/en.json`]);
-    console.log('>>> Translation added!');
-    askTranslation();
+    // Handle already existing key
+    if (dataFr[key] || dataEn[key]) {
+      inquirer.prompt([{
+        type: 'confirm',
+        name: 'confirmReplace',
+        message: 'Key already exists in files. Replace?'
+      }]).then(confirmRes => {
+        if (confirmRes.confirmReplace) {
+          addTranslationInFiles(key, res['en'], res['fr'])
+        } else {
+          askTranslation()
+        }
+      })
+    }
+    // Key doesn't exists in translations files
+    else {
+      addTranslationInFiles(key, res['en'], res['fr'])
+    }
+
   })
-};
-askTranslation();
+}
+
+const addTranslationInFiles = (key, enTranslation, frTranslation) => {
+  dataEn[key] = enTranslation
+  dataFr[key] = frTranslation
+  fs.writeFileSync(`${translationFilesLocation}/fr.json`, JSON.stringify(dataFr))
+  fs.writeFileSync(`${translationFilesLocation}/en.json`, JSON.stringify(dataEn))
+  sortJson.overwrite([`${translationFilesLocation}/fr.json`, `${translationFilesLocation}/en.json`])
+  console.log('>>> Translation added!')
+  askTranslation()
+}
+
+// Init app
+askTranslation()
